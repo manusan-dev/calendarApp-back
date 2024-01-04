af951a1f28c32537d8ed575e8f9b053e5cf064c5
Manuel Sánchez <msanchez@ivolutia.com>
2024-01-04 12:08:07 -0300

    update 1

--- index.js
+++ index.js
@@ -38,6 +38,6 @@ app.use('/api/events', require('./routes/events'));
 
 // escuchar peticiones
  
-app.listen( process.env.port, () => {
-    console.log(`Servidor corriendo en puerto ${ process.env.port }` );
+app.listen( process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${ process.env.PORT }` );
 } );
--- models/Evento.js
+++ models/Evento.js
@@ -26,10 +26,10 @@ const EventoSchema = Schema({
 
 });
 
-EventoSchema.method('toJSON', function(){
-    const {__v, _id, ...object} = this.toObject();
-    object.id = _id;
-    return object;
+EventoSchema.method('toJSON', function() {
+  const { __v, _id, ...object } = this.toObject();
+  object.id = _id;
+  return object;
 });
 
 
